50c1f3d220db3941f1c965516bb021c6554f3544
bybang <bybang26@gmail.com>
2021-12-22 13:40:43 -0500

    Added function that fetch data from database in loadTweets func

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,52 @@
  */
 $(document).ready(() => {
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+const $form = $('#submit-new-tweet');
 
+$form.submit(function(event) {
+  event.preventDefault();
+  const serializedData = $(this).serialize();
+  $.post('/tweets', serializedData)
+});
+
+const loadTweets = () => {
+  $.ajax({
+    url:'/tweets',
+    method: 'GET',
+    dataType: 'JSON',
+  })
+  .then(function(tweets) {
+    renderTweets(tweets);
+  })
+};
+
+loadTweets()
+
+// Fake data taken from initial-tweets.json
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const createTweetElement = (newTweet) => {
   const $tweet = $('<article>').addClass('tweetArticle');
@@ -58,59 +78,10 @@ const createTweetElement = (newTweet) => {
 const renderTweets = function(tweets) {
   const $tweetContainer = $('#tweets-container');
   $tweetContainer.empty();
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet)
-    $tweetContainer.prepend($tweet);
-  } 
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-}
-
-renderTweets(data);
-
-const $form = $('#submit-new-tweet');
-
-$form.submit(function(event) {
-  // console.log(event);
-  event.preventDefault();
-  const serializedData = $(this).serialize();
-
-  $.ajax({
-    type: 'POST',
-    url: '/tweets',
-    data: serializedData,
-  });
-  
-  // $.post('/tweets', serializedData, (response) => {
-  //   console.log(serializedData);
-  //   console.log(response);
-  // })
-});
-
-const loadTweets = () => {
-  $.ajax({
-    url:'/tweets',
-    method: 'GET',
-    dataType: 'JSON',
-  })
+  for (const tweet of tweets) { // loops through tweets
+    const $tweet = createTweetElement(tweet) // calls createTweetElement for each tweet
+    $tweetContainer.prepend($tweet); // takes return value and appends it to the tweets container
+  }
 };
-
-// const fetchBlogs = () => {
-//   $.ajax({
-//     url: '/api/blogs',
-//     method: 'GET',
-//     dataType: 'json',
-//     success: (blogs) => {
-//       console.log("data", blogs);
-//       createBlogs(blogs);
-//     },
-//     error: (err) => {
-//       console.log(`error: ${err}`)
-//     } 
-//   });
-// };
-
-// fetchBlogs();
 
 });
